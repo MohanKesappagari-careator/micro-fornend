fa1ef129c5eac628e511a1ea0cc6a290d8b7ad8a
MohanKesappagari-careator <mohan.k@careator.com>
2021-11-11 14:23:21 +0530

    first commit

---
+++ home/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
+import "./index.scss";
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-9 text-center">Main</div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ home/src/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="p-4 text-white bg-indigo-500 text-center text-3xl text-bold">
+      Footer
+    </div>
+  );
+}
